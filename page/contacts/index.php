d611a52e78ca88370146070f8f16be983a8478fd
DESKTOP-IOIR1A1\Arey <arey.9999@gmail.com>
2020-08-17 23:13:33 +0300

    added contacts html and css

--- page/contacts/index.php
+++ page/contacts/index.php
@@ -14,19 +14,63 @@
 </div>
 
 <main id="contacts-content">
-    <div class="container">
-        <div class="error-block">
-            <p class="error-text">
-                Извините данной страницы не существует
-            </p>
-            <a class="btn-white" href="<?php href('home') ?>">Перейти на главную</a>
+        <div class="contacts-block">
+            <div class="contacts-info">
+                <div class="contacts-title">Наши контакты</div>
+
+                <div class="contacts-group contacts-email">
+                    <a href="mailto: spaceonline@gmail.com">SpaceOnline@gmail.com</a>
+                </div>
+
+                <div class="contacts-group contacts-phone">
+                    <a href="tel:+380766443274">+38 (076) 644 32 74</a>
+                    <br>
+                    <a href="tel:+380766443274">+38 (076) 644 32 74</a>
+                </div>
+
+                <div class="contacts-group contacts-timetable">
+                    График работы:<br>
+                    ПН-ПТ, 10:00 - 19:00<br>
+                    <small>[UTC/GMT+3] Москва</small>
+                </div>
+            </div>
+
+            <div class="contacts-form">
+                <div class="contacts-title">Отправить нам письмо</div>
+
+                <form action="" method="post">
+                    <div class="contacts-group-input">
+                        <input type="text" placeholder="Ваше имя" name="name">
+                        <span class="error-text">Некорректные данные!</span>
+                    </div>
+
+                    <div class="contacts-group-input">
+                        <input type="text" placeholder="Телефон" name="phone">
+                        <span class="error-text">Некорректные данные!</span>
+                    </div>
+
+                    <p class="contacts-separator"></p>
+
+                    <div class="contacts-group-input">
+                        <input type="text" placeholder="Email" name="email">
+                        <span class="error-text"></span>
+                    </div>
+
+                    <div class="contacts-group-input">
+                        <input type="text" placeholder="Тема" name="theme">
+                        <span class="error-text"></span>
+                    </div>
+
+                    <p class="label">Ваше сообщение</p>
+
+                    <textarea name="" id="" cols="30" rows="10"></textarea>
+
+                    <button type="submit">Отправить</button>
+                </form>
+            </div>
         </div>
     </div>
 </main>
-
-<style>
-
-</style>
 
 <?php put('footer') ?>
 
